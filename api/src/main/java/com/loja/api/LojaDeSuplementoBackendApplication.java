0a2969e31738433e3e30f69dfe6c70f4456f7322
Felipe Pereira <felipejiti@hotmail.com>
2023-09-05 14:36:02 -0300

    inicio do projeto

---
+++ api/src/main/java/com/loja/api/LojaDeSuplementoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.loja.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaDeSuplementoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaDeSuplementoBackendApplication.class, args);
+	}
+
+}
