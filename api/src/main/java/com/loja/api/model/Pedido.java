abeef8c046ba9cdae1b466a502c45a799143dace
Felipe Pereira <felipejiti@hotmail.com>
2023-09-18 15:31:57 -0300

    Refatoração na entidade Produto

--- api/src/main/java/com/loja/api/model/Pedido.java
+++ api/src/main/java/com/loja/api/model/Pedido.java
@@ -5,15 +5,19 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 @Entity
-public class Pedido {
+public class Pedido implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+    private String descricao;
 
     @OneToOne
     @JoinColumn(name = "cliente_id")
